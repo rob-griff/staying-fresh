c1aaa2742c554162229236d9f6d825e945810fac
Robin Griffiths <robin.griffiths@proton.me>
2024-07-12 07:47:17 +0000

    day 2

---
+++ Python/thirty_days_of_python/day_02/variables.py
@@ -0,0 +1,39 @@
+first_name = 'Robin'
+country = 'Australia'
+is_true = True
+
+dessert, flavour, cost = 'ice cream', 'vanilla', 9.81
+
+print(first_name)
+print(country)
+print(is_true)
+print(dessert)
+print(flavour)
+print(cost)
+
+print(type(dessert))
+print(type(cost))
+
+print(len(first_name))
+print(len(country))
+
+num_1 = 5
+num_2 = 4
+
+total = num_1 + num_2
+diff = num_2 - num_1
+
+radius = 30
+area_of_circle = 3.1415*radius**2
+circum_of_circle = 2*3.1415*radius
+
+radius = float(input('Enter radius: '))
+area_of_circle = 3.1415*radius**2
+print(area_of_circle)
+
+# User Input
+name = input('Enter your name: ')
+age = int(input('Enter your age: '))
+
+
+print(name, " " ,age)
